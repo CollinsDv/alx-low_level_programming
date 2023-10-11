72e0ed56f7efa5a5c829d0c249669c5e0c206bc8
Collins Kimani <mwas9446.ck@gmail.com>
2023-10-11 10:43:17 -0700

    update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,31 +1,20 @@
 #include "main.h"
 
 /**
- * main - calls print_alphabet
- *
- * Return: Suvess (0)
- */
-int main(void)
-{
-	print_alphabet_x10();
-
-	return (0);
-}
-
-/**
  * print_alphabet_x10 - prints alphabet 10 times
  *
  * Return: void
  */
 void print_alphabet_x10(void)
 {
-	int i,j;
+	int i, j;
+
 	i = 0;
 
 	while (i < 10)
 	{
 		j = 'a';
-		
+
 		while (j <= 'z')
 		{
 			_putchar(j);
