f2e8917ec779d3afaecaff75f79ffd6f5c21ae71
Collins Kimani <mwas9446.ck@gmail.com>
2023-10-11 23:10:32 -0700

    add readme and file 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - Determines case of an input
+ *
+ * @c: arguement 1
+ * Return: 1 for uppercase, 2 for lowercase
+ */
+int _isupper(int c)
+{
+	int c;
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
