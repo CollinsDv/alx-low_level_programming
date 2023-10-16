5dff25ddd9864c99b1ff3381994089fc9d7497de
Collins Kimani <mwas9446.ck@gmail.com>
2023-10-16 15:37:56 -0700

    add file 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prints numbers from start and every other
+ * @str: param1
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i, len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 == 0)
+			continue;
+		else
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
