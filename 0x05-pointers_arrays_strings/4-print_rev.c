ffef5ce9877d4ed1eba16bf0ca01cda30b6c7ffe
Collins Kimani <mwas9446.ck@gmail.com>
2023-10-16 12:59:30 -0700

    added file 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - reverses a string and prints
+ *
+ * @s: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int x, len = strlen(s);
+
+	for (x = len; x > 1; x--)
+	{
+		_putchar(s[x]);
+	}
+	_putchar('\n');
+}
