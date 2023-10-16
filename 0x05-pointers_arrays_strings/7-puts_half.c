04df3f135f041f27a637d22e1d5df3e220e4f37b
Collins Kimani <mwas9446.ck@gmail.com>
2023-10-16 16:16:55 -0700

    add file 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints right half digit length
+ * @str: string parameter
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, index_one, len = strlen(str);
+
+	if (len % 2 == 0)
+		index_one = len / 2;
+	else
+		index_one = (len - 1) / 2;
+
+	for (i = index_one; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
