843cd4dcbf21cc3eca40398cae1c77dc286aa4c8
Collins Kimani <mwas9446.ck@gmail.com>
2023-10-16 11:50:04 -0700

    add file 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - print the length string
+ * @s: param1
+ * Return: int value of string length
+ */
+
+int _strlen(char *s)
+{
+	char str[] = s;
+
+	return (strlen(str));
+}
