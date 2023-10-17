ef33e80dfe60b44685b483bc96ab018a82a353e6
Collins Kimani <mwas9446.ck@gmail.com>
2023-10-16 19:35:19 -0700

    updated file 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,7 +15,7 @@ void print_array(int *a, int n)
 		printf("\n");
 	}
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (i == 0)
 			printf("%d,", a[i]);
@@ -23,5 +23,4 @@ void print_array(int *a, int n)
 			printf(" %d,", a[i]);
 
 	}
-	printf("%d\n", a[n - 1]);
 }
