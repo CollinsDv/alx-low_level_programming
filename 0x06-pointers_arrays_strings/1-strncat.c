ae3cb7910161fa01b91ccc3691ccce75e32d5f79
Collins Kimani <mwas9446.ck@gmail.com>
2023-10-18 13:55:35 -0700

    update file 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,25 @@
 #include "main.h"
 
+/**
+ * _strncat - joins two strings
+ *
+ * Description: generally prints value of src
+ * and places null termiator
+ * @dest: param1
+ * @src: param2
+ * @n: param3
+ * Return: a string
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, *ptr;
-	
+	int i;
+	char *ptr;
+
 	ptr = dest;
 	while (*dest != '\0')
+	{
 		ptr++;
-	
+	}
 	i = 0;
 
 	while (i < n && i != '\0')
@@ -19,5 +31,5 @@ char *_strncat(char *dest, char *src, int n)
 	if (i < n)
 		*dest = '\0';
 
-	return (prt);
+	return (ptr);
 }
