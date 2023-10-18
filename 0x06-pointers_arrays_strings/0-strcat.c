119d5e6f3b0bf283f04185367f7f34ff4e9e2b31
Collins Kimani <mwas9446.ck@gmail.com>
2023-10-18 10:46:55 -0700

    add file 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - joins two strings together
+ *
+ * @src: param2
+ * @dest: param1
+ * Return: two combined strings
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *ptr;
+
+	ptr = dest;
+
+	while (*ptr != '\0')
+		ptr++;
+
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+	*dest = '\0';
+	return (*ptr);
+}
