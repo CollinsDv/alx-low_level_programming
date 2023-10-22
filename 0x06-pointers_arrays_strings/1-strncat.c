9bbf46844917d4155ba89bcfa33fdc3cf310948f
Collins.M <68195455+CollinsDv@users.noreply.github.com>
2023-10-22 11:27:28 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	char *ptr;
-
+	
 	ptr = dest;
 	while (*ptr != '\0')
 	{
@@ -22,15 +22,14 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	i = 0;
 
-	while (i < n && i != '\0')
+	while (i < n && *src != '\0')
 	{
 		*ptr = *src;
 		ptr++;
 		src++;
 		i++;
 	}
-	if (i < n)
-		*ptr = '\0';
+	*ptr = '\0';
 
 	return (dest);
 }
