67a2dabb026e68bcce0ed1ad9dde76c3e454bdc9
Collins Kimani <mwas9446.ck@gmail.com>
2023-10-23 13:52:32 -0700

    add file 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of prefix substring
+ * @s: parameter 1
+ * @accept: parameter 2
+ * Return: an interger of similar character occurences
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, counter;
+
+	counter = 0;
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0' ; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				counter++;
+				break;
+			}
+		}
+	}
+	return (counter + 1);
+}
