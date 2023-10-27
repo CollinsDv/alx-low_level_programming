0b2fb01ec8463e11c462e5d623527747284edfc0
Collins Kimani <mwas9446.ck@gmail.com>
2023-10-27 06:19:59 -0700

    update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Find all .c files in the current directory and its subdirectories
-find . -name ".c" -exec gcc -c -Wall -Werror -Wextra {} +
+find . -name '*.c' -exec gcc -c -Wall -Werror -Wextra {} +
 
 # Create a static library named liball.a from the generated .o files
 ar -rcs liball.a '*.o'
