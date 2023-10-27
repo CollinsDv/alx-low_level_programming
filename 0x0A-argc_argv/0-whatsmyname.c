cc9d5a2c1951f97277a5ff666502895d6ec21908
Collins Kimani <mwas9446.ck@gmail.com>
2023-10-27 12:41:27 -0700

    update file 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,24 +1,17 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - a program that prints its name
- *
+ * 
+ * Description: doesn't need another compilation to read
+ * new program name
+ * @argc: param1
+ * @argv[]: array of charater pointers
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int i = 0;
-	char *ptr;
-
-	for(; i < argc; i++)
-	{
-		ptr = argv[i];
-		while (*ptr != '\0')
-		{
-			_putchar(*ptr);
-			ptr++;
-		}
-	}
-	_putchar('\n');
+	printf("%s\n", argv[0]);
+	(void)argc;
 	return (0);
 }
