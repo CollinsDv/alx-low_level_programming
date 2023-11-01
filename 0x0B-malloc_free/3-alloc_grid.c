c84578c88594e79ff670c0fc602232b7d45678ae
Collins.M <68195455+CollinsDv@users.noreply.github.com>
2023-11-01 19:43:22 +0300

    Update 3-alloc_grid.c
    refactored the if statement after allocating the dynamic array to the number of rows to populate the 2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,46 +3,38 @@
 #include "main.h"
 
 /**
- * alloc_grid - prints a grid sized width x height
- * populated with 0
+ * alloc - prints a grid sized width x height 
+ * populated with 0 * 
  * @width: 1st array subscript
- * @height: 2nd array subscript
+ * @height: 2nd array subscript 
  * Return: pointer or NULL
  */
-int **alloc_grid(int width, int height) /* 6 columns and 4 rows*/
+int **alloc_grid(int width, int height)
 {
-	int i, j;
-	int **array;
-
-	if (width < 1 || height < 1)
-		return (NULL);
-
-	array = (int **)malloc(sizeof(int *) * height);
-	if (array == NULL)
-	{
-		free(array);
-		return (NULL);
-	}
-
-	for (i = 0; i < height; i++)
-	{
-		array[i] = malloc(sizeof(int) * width);
-		if (array[i] == NULL)
-		{
-			for (i = 0; i < height; i++)
-			{
-				free(array[i]);
-			}
-			free(array);
-			return (NULL);
-		}
-	}
-
-	for (i = 0; i < height; i++)
-		for (j = 0; j < width; j++)
-		{
-			array[i][j] = 0;
-		}
-
-	return (array);
+        int c, r, i;
+        int **array;
+        
+        if (width == 0 || height == 0)
+                return (NULL);
+        /*make multidimensional 2D array*/
+        array = (int **)malloc(height * sizeof(*array));
+        if (array == NULL)
+                return (NULL);
+        
+        for (r = 0; r < height; r++)
+        {
+                array[r] = malloc(width * sizeof(**array));
+                if (array[r] == NULL)
+                {
+                        for ( i = 0; i < r; i++)
+                        {
+                                free(array[i]);
+                        }
+                        free(array);
+                        return NULL;
+                }
+                for (c = 0; c < width; c++)
+                        array[r][c] = 0;
+        }                
+        return (array);
 }
