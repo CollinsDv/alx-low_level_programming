044610986d11049b5642ed13da55fba75e067646
Collins Kimani <mwas9446.ck@gmail.com>
2023-11-08 14:08:41 -0800

    add all files 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op-add - adds two integers
+ * @a: integer1
+ * @b: integer2
+ * Return: sum integer
+ */
+int op_add(int a, int b)
+{
+        return (a + b);
+}
+/**
+ * op_sub - returns difference of two numbers
+ * @a: integer1
+ * @b: integer2
+ * Return: difference integer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - returns multiplication of two numbers
+ * @a: integer1
+ * @b: integer2
+ * Return: multiplication integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns division of two numbers
+ * @a: integer1
+ * @b: integer2
+ * Return: division integer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns modulo of two numbers
+ * @a: integer1
+ * @b: integer2
+ * Return: modulus integer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
