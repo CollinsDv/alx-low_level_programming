57373cb4d47c91bc857d01d6b4d8c083ac2fcaaf
Collins Kimani <mwas9446.ck@gmail.com>
2023-11-09 06:30:42 -0800

    update file 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,15 +11,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i, elem;
 	va_list ptr;
 
-	if (separator == NULL || n == 0)
+	if (n == 0)
 		return;
 
 	va_start(ptr, n);
 	for (i = 0; i < n; i++)
 	{
 		elem = va_arg(ptr, int);
-		if (i < (n - 1))
-			printf("%d %c", elem, *separator);
+		printf("%d", elem);
+
+		if ((i < (n - 1)) && (separator != NULL))
+			printf("%c", *separator);
 	}
-	printf("%d\n", elem);
+	printf("\n");
 }
