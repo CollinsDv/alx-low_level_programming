4d2b4634a6248f52d2b8d4f99905308905bca79b
Collins Kimani <mwas9446.ck@gmail.com>
2023-11-09 08:46:57 -0800

    update file 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,14 +11,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list ptr;
-	char *string;
+	const char *string;
 
 	va_start(ptr, n);
 	for (i = 0; i < n; i++)
 	{
-		string = va_arg(ptr, char *);
+		string = va_arg(ptr, const char *);
 		if (string == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", string);
 
