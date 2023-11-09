f833981143f22f4664a85755a7eea0c82fa067fd
Collins Kimani <mwas9446.ck@gmail.com>
2023-11-09 03:50:29 -0800

    update file 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,14 +7,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum = 0;
+	unsigned int i, sum;
+	va_list ptr; /* pointer to last variadic arguement*/
 
 	if (n == 0)
 		return (0);
 
-	va_list ptr; /* pointer to last variadic arguement*/
-
 	va_start(ptr, n);
+	sum = 0;
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ptr, int);
