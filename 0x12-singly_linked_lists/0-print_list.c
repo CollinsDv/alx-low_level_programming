17117ea02f64a39d5062a3e02754af44367315ba
Collins Kimani <mwas9446.ck@gmail.com>
2023-11-16 09:40:51 -0800

    update file 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,6 +10,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t nodes;
+	int len;
 
 	if (h == NULL)
 		return (0);
@@ -18,9 +19,11 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		nodes++;
+		len = strlen(h->str);
 		if (!h->str)
 			printf("[0] (nil)");
-		printf("%s", h->str);
+		else
+			printf("[%d] %s\n", len, h->str);
 		h = h->next;
 	}
 	return (nodes);
