d4e740b32845714c721c9a89f4a3c4210a4f1fd4
Collins Kimani <mwas9446.ck@gmail.com>
2023-11-20 10:49:15 -0800

    update file 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp = NULL;
 	new_node = NULL;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(*new_node));
