296ca61c8571caaa6655919508496e4baa4b0789
Collins Kimani <mwas9446.ck@gmail.com>
2023-11-21 09:40:01 -0800

    update file 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,6 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			if (temp == NULL)
 				return (NULL);
 			temp = temp->next;
+			i++;
 		}
 		if (temp->next == NULL)
 			temp->next = new_node;
