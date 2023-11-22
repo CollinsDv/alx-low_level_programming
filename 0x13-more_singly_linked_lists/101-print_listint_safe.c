7ca867105f7e806f7b8e9df92f6753136e922506
Collins Kimani <mwas9446.ck@gmail.com>
2023-11-22 06:17:34 -0800

    update file 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,9 +12,18 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t nodes;
 	const listint_t *temp = NULL;
+	const listint_t *loop1 = head, *loop2 = head;
 
 	if (head == NULL)
-		return (0);
+		return (98);
+	while (loop1 != NULL || loop2 != NULL || loop2->next != NULL)
+	{
+		loop1 = loop1->next;
+		loop2 = loop2->next->next;
+
+		if (loop1 == loop2)
+			exit(98);
+	}
 
 	nodes = 0;
 	temp = head;
