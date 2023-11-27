409c82bcc1c902126299e7f403676b39cc598830
Collins Kimani <mwas9446.ck@gmail.com>
2023-11-27 12:08:59 -0800

    add file 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - creates a new file and writes content in it
+ *
+ * @filename: the file to be created
+ * @text_content: text to be filled in the file
+ *
+ * Return: interger of NULL
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, fd1;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+	if (fd < 0)
+		return (-1);
+
+	fd1 = write(fd, text_content, strlen(text_content));
+	if (fd1 < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
