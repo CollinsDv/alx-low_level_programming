e4cc551f917e1d81f6828819fc2eaf17867f19bc
Collins Kimani <mwas9446.ck@gmail.com>
2023-11-27 15:50:15 -0800

    update file 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL);
+	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 
