3aa162b2e394a8e68544e4505a3a52c1b79ae518
Collins Kimani <mwas9446.ck@gmail.com>
2023-11-27 10:17:30 -0800

    update file 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,27 +10,33 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, len, readfd;
-
+	int fd, readfd;
 	char *readfile;
 
 	if (filename == NULL || letters == 0)
 		return (0);
 
-	len = strlen(filename);
-
-	readfile = malloc(sizeof(*readfile) * len);
+	readfile = malloc(sizeof(*readfile) * (letters + 1));
 	if (readfile == NULL)
 		return (0);
 
 	fd = open("filename", O_RDONLY);
 	if (fd < 0)
+	{
+		free(readfile);
 		return (0);
+	}
 
 	readfd = read(fd, readfile, letters);
-	readfile[len] = '\0';
+	if (readfd < 0)
+	{
+		free(readfile);
+		close(fd);
+		return (0);
+	}
+	readfile[readfd] = '\0';
 
-	write(1, readfile, letters);
+	write(1, readfile, readfd);
 	free(readfile);
 
 	close(fd);
