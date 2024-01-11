559a3bf4548b48e1d9683d07589de370289261ad
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-11 22:56:04 +0300

    add file 6 and 7

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/*
+ * sum_dlistint - returns sum of doubly linked list elements
+ *
+ * @head: head of the linked list
+ *
+ * @Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	size_t sum = 0;
+
+	if (head == NULL)
+		return (0);
+	
+	temp = head;
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,77 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert node at idx
+ *
+ * @h: doubly linked list head node
+ * @idx: index position
+ * @n: data to input in list
+ *
+ * Return: updated list
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp = *h, *new_node; 
+	unsigned int count = 0, size = dlistint_len(*h);
+
+	if (idx > size - 1 || h == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+	{
+		dprintf(2, "ERROR: malloc error\n");
+		return NULL;
+	}
+	new_node->n = n;
+	while (count < idx)
+	{
+		temp = temp->next;
+		count++;
+	}
+	if (temp->prev == NULL)
+	{
+		new_node->next = *h;
+		new_node->prev = NULL;
+		if (temp != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+	}
+	else if (temp->next == NULL)
+	{
+		new_node->next = NULL;
+		new_node->prev = temp;
+		temp->next = new_node;
+	}
+	else
+	{
+		new_node->next = temp;
+		temp->prev->next = new_node;
+		temp->prev = new_node;
+	}
+	return (temp->prev);
+}
+
+/**
+ * dlistint_len - return no. of elements of a list
+ *
+ * @h: head node pointer
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp = NULL;
+	size_t nodes = 0;
+
+	if (!h)
+		return (0);
+
+	temp = h;
+	while (temp)
+	{
+		nodes++;
+		temp = temp->next;
+	}
+	return (nodes);
+}
