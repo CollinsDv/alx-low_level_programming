06b8853713c1c1ae8098c3d11b5aab7b53fe31fe
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-11 23:02:17 +0300

    handle betty errors

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-/*
+/**
  * sum_dlistint - returns sum of doubly linked list elements
  *
  * @head: head of the linked list
@@ -14,7 +14,7 @@ int sum_dlistint(dlistint_t *head)
 
 	if (head == NULL)
 		return (0);
-	
+
 	temp = head;
 	while (temp != NULL)
 	{
