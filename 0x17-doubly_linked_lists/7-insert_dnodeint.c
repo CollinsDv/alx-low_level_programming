79f447dd84847d81642337f2ac6ca867e59dfcc4
Collins.M <68195455+CollinsDv@users.noreply.github.com>
2024-01-13 16:03:53 +0300

    Update 7-insert_dnodeint.c
    tried a different approach

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,41 +11,50 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp = *h, *new_node = malloc(sizeof(*new_node));
-	unsigned int count = 0, size = dlistint_len(*h);
+    dlistint_t *new_node, *temp;
+    unsigned int i;
 
-	if (idx < 0 || idx > size - 1 || h == NULL || new_node == NULL)
-		return (NULL);
+    if (h == NULL)
+        return NULL;
 
-	new_node->n = n;
-	while (count < idx)
-	{
-		temp = temp->next;
-		count++;
-	}
-	if (temp->prev == NULL) /* add node as head node */
-	{
-		new_node->next = temp;
-		new_node->prev = NULL;
-		if (temp != NULL)
-			temp->prev = new_node;
-		*h = new_node;
-		return (temp);
-	}
-	else if (temp->next == NULL) /* add node as tail node */
-	{
-		new_node->next = NULL;
-		new_node->prev = temp;
-		temp->next = new_node;
-		return (temp->next);
-	}
-	else
-	{
-		new_node->next = temp;
-		temp->prev->next = new_node;
-		temp->prev = new_node;
-		return (temp->prev);
-	}
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return NULL;
+
+    new_node->n = n;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+
+    if (idx == 0)
+    {
+        new_node->next = *h;
+        if (*h != NULL)
+            (*h)->prev = new_node;
+        *h = new_node;
+        return new_node;
+    }
+
+    temp = *h;
+    for (i = 0; i < idx - 1 && temp != NULL; i++)
+    {
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+    {
+        free(new_node);
+        return NULL;
+    }
+
+    new_node->next = temp->next;
+    new_node->prev = temp;
+
+    if (temp->next != NULL)
+        temp->next->prev = new_node;
+
+    temp->next = new_node;
+
+    return new_node;
 }
 
 /**
