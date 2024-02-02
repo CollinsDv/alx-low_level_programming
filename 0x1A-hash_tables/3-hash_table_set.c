ff4411493897fca9613f6bdad9213996d5ad4c0a
Collins Kimani <mwas9446.ck@gmail.com>
2024-02-02 18:37:55 +0300

    add file 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,63 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - inserts an element into a hash table
+ *
+ * @ht: hash table
+ * @key: key
+ * @value: value
+ * Return: success 1 or error 0
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new = NULL, *current = NULL;
+
+	if (key == NULL || value == NULL || ht == NULL || key[0] == '\0')
+		return (0);
+	/* generate hash code then feed it to key index func to obtain index */
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return (EXIT_SUCCESS);
+		}
+		current = current->next;
+	}
+	/* generate the element */
+	new = create_hash_node(key, value);
+
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (EXIT_SUCCESS);
+}
+
+/**
+ * create_hash_node - create a hash_node item
+ *
+ * @key: key
+ * @value: value
+ *
+ * Return: On success, a new hash_node item
+ */
+hash_node_t *create_hash_node(const char *key, const char *value)
+{
+	hash_node_t *hash_node = malloc(sizeof(*hash_node));
+	if (hash_node == NULL)
+	{
+		dprintf(2, "malloc error\n");
+		exit(EXIT_FAILURE);
+	}
+	hash_node->key = strdup(key);
+	hash_node->value = strdup(value);
+	hash_node->next = NULL;
+
+	return (hash_node);
+}
