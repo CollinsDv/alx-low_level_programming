2a08e217dad9773091b845c7a781137b87a356f9
Collins Kimani <mwas9446.ck@gmail.com>
2024-02-02 20:38:40 +0300

    file 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,15 +1,19 @@
 #include "hash_tables.h"
 /**
- * hash_table_get - retrieves a value associated with a key
- * @ht: hash table to look into
- * @key: key to look for
- * Return: value associated with the element, or
- * NULL if key couldn’t be found
+ * hash_table_get - retrieves a value mapped to a key
+ *
+ * @ht: hash table
+ * @key: key to get value
+ *
+ * Return: value associated with the key or NULL
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
-	hash_table_t *current = NULL;
+	hash_table_s *current = NULL;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
