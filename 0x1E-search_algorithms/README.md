8b2e1fe842f16cd0a2cb77f8b966974b1cc4036d
Collins Kimani <mwas9446.ck@gmail.com>
2024-05-07 05:44:02 +0300

    add file 0 and readme

---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,2 @@
+# Search algorithms
+This is an research based on steps of defining towards a search problem while looking at their efficiency in the long run, based on time and space complexity.
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+# ifndef SEARCH_ALGO
+# define SEARCH_ALGO
+
+/* system headers */
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/* func prototypes */
+int linear_search(int *array, size_t size, int value);
+
+# endif
